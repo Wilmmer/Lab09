ef0214961a85c8b7b681e576db9d6ecde4057dc7
LAPTOP-J2LNH6VD\Lenovo <wilmmer_052@live.com.mx>
2017-06-22 00:14:22 -0500

    Laboratorio 11 Resuelto

---
+++ Lab11/Lab11/MainActivity.cs
@@ -0,0 +1,129 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab11
+{
+    [Activity(Label = "Lab11", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Complex Data;
+        Validate Dato;
+        int Counter = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            Android.Util.Log.Debug("Lab11Log","Activity A - OnCreate");
+            
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.StartActivity).Click += (sender, e) =>
+            {
+                var ActivityIntent = new Android.Content.Intent(this, typeof(SecondActivity));
+                StartActivity(ActivityIntent);
+            };
+
+            //Utilizar FragmentManager para recuperar el Fragmento
+            Data = (Complex)this.FragmentManager.FindFragmentByTag("Data");
+        
+            if(Data == null)
+            {
+                // No ha sido almacenado, agregar el fragmento a la Activity
+                Data = new Complex();
+                var FragmentTransation = this.FragmentManager.BeginTransaction();
+                FragmentTransation.Add(Data, "Data");
+                FragmentTransation.Commit();
+
+            }
+
+
+            if(bundle!= null)
+            {
+                Counter = bundle.GetInt("CounterValue",0);
+                Android.Util.Log.Debug("Lab11Log", "Activity A - Recovered Instance State");
+            }
+
+            var ClickCounter = FindViewById<Button>(Resource.Id.ClicksCounter);
+            ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+
+            ClickCounter.Text += $"\n{Data.ToString()}";
+
+            ClickCounter.Click += (sender, e) =>
+            {
+                Counter++;
+                ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+
+                // Modificar con cualquier valor solo para verificar la persistencia
+                Data.Real++;
+                Data.Imaginary++;
+                //Mostrar el valor de los miembros
+                ClickCounter.Text += $"\n{Data.ToString()}";
+            };
+
+            ////////////////////////////
+            Validar();
+
+        }
+        private async void Validar() {
+
+            var TextContent = FindViewById<TextView>(Resource.Id.textContenido);
+            Dato = (Validate)this.FragmentManager.FindFragmentByTag("Dato");
+            if (Dato == null)
+            {
+                Dato = new Validate();
+                var FragmentTransation = this.FragmentManager.BeginTransaction();
+                FragmentTransation.Add(Dato, "Dato");
+                FragmentTransation.Commit();
+                string Email = "wilmmer_052@live.com.mx";
+                string Password = "**********";
+                string MyDevice = Android.Provider.Settings.Secure.GetString
+                    (ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+                SALLab11.ServiceClient ServiceClient = new SALLab11.ServiceClient();
+                Dato.Result = await ServiceClient.ValidateAsync(Email, Password, MyDevice);
+            }
+            TextContent.Text = $"{Dato.ToString()}";
+
+        }
+        protected override void OnStart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnStart");
+            base.OnStart();
+        }
+        protected override void OnResume()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnResume()");
+            base.OnResume();
+        }
+        protected override void OnPause()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnPause()");
+            base.OnPause();
+        }
+        protected override void OnStop()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnStop()");
+            base.OnStop();
+        }
+        protected override void OnDestroy()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnDestroy()");
+            base.OnDestroy();
+        }
+        protected override void OnRestart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnRestart()");
+            base.OnRestart();
+        }
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            outState.PutInt("CounterValue", Counter);
+            Android.Util.Log.Debug("Lab11Log","Activity A - OnSaveInstanceState");
+
+            base.OnSaveInstanceState(outState);
+        }
+        
+    }
+}
+
---
+++ Lab11/Lab11/Validate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Lab11
+{
+    public class Validate: Fragment
+    {
+        public SALLab11.ResultInfo Result { get; set; }
+        public override string ToString()
+        {
+            return $"{Result.Status}\n{Result.Fullname}\n{Result.Token}";
+        }
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            RetainInstance = true;
+        }
+    }
+}
